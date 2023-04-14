7ba780c6609402f804e1db6e99f5a7ac0ed674a6
Douglas Sales <douglas.sales@multipedidos.com.br>
2023-04-14 19:41:50 -0300

    Add validate

--- app/Services/AppointmentService.php
+++ app/Services/AppointmentService.php
@@ -3,11 +3,26 @@
 namespace App\Services;
 
 use App\Repositories\AppointmentRepository;
+use Illuminate\Http\Request;
 
 class AppointmentService extends TecmidService
 {
     public function __construct()
     {
         parent::__construct(new AppointmentRepository());
+    }
+
+    /**
+     * Insert data on database
+     * 
+     * @param Request $request
+     */
+    public function create(Request $request)
+    {
+        $request->validate([
+            'doctor_id' => 'integer|required'
+        ]);
+
+        return parent::create($request);
     }
 }
