83c073048248561310a24e2733bccf96448e6eed
Douglas Sales <douglas.sales@multipedidos.com.br>
2023-04-14 00:35:38 -0300

    Create patient model

---
+++ app/Models/Patient.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+class Patient
+{
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'name',
+        'cpf',
+        'email',
+        'phone',
+        'birth_date',
+        'gender',
+        'notes',
+        'address_street',
+        'address_number',
+        'address_complement',
+        'address_postal_code',
+        'address_neighborhood',
+        'address_city',
+        'address_state',
+        'address_country',
+    ];
+
+}
