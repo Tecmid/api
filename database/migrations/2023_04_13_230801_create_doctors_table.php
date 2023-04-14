0487068bd40b51195268e5adda052c145195dd30
Douglas Sales <douglas.sales@multipedidos.com.br>
2023-04-14 00:11:43 -0300

    Create doctors table

---
+++ database/migrations/2023_04_13_230801_create_doctors_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('doctors', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->timestamp('email_verified_at')->nullable();
+            $table->string('password');
+            $table->string('cpf', 11)->unique();
+            $table->string('phone');
+            $table->dateTime('birth_date')->nullable();
+            $table->string('address_street');
+            $table->integer('address_number');
+            $table->string('address_complement')->nullable();
+            $table->string('address_postal_code');
+            $table->string('address_neighborhood');
+            $table->string('address_city');
+            $table->string('address_state');
+            $table->string('address_country');
+            $table->json('specialties')->nullable();
+            $table->text('image');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('doctors');
+    }
+};
