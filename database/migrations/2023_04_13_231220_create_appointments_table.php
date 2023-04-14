d90dae9aaa5f69ae0e11e514cad4d95acd88c876
Douglas Sales <douglas.sales@multipedidos.com.br>
2023-04-14 00:11:52 -0300

    Create appointments table

---
+++ database/migrations/2023_04_13_231220_create_appointments_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('appointments', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('doctor_id');
+            $table->unsignedBigInteger('patient_id');
+            $table->integer('anamnese_id');
+            $table->dateTime('date');
+            $table->integer('status');
+            $table->integer('payment_status');
+            $table->text('notes')->nullable();
+            $table->string('duration')->nullable();
+            $table->json('images')->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+
+            $table->foreign('doctor_id')->references('id')->on('doctors');
+            $table->foreign('patient_id')->references('id')->on('patients');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('appointments');
+    }
+};
