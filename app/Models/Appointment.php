ebd1880398cb045cbe9a414308c7ed6d475e6a78
Douglas Sales <douglas.sales@multipedidos.com.br>
2023-04-14 00:35:59 -0300

    Create appointment model

---
+++ app/Models/Appointment.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Models;
+
+class Appointment
+{
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'doctor_id',
+        'patient_id',
+        'anamnese_id',
+        'date',
+        'status',
+        'payment_status',
+        'notes',
+        'duration',
+        'images',
+    ];
+
+    /**
+     * Get the doctor associated with the appointment
+     * 
+     * @return HasOne
+     */
+    public function doctor(): HasOne
+    {
+        return $this->hasOne(Doctor::class, 'doctor_id');
+    }
+
+    /**
+     * Get the patient associated with the appointment
+     * 
+     * @return HasOne
+     */
+    public function patient(): HasOne
+    {
+        return $this->hasOne(Patient::class, 'patient_id');
+    }
+
+    /**
+     * Get the anamnese associated with the appointment
+     * 
+     * @return HasOne
+     */
+    public function anamnese(): HasOne
+    {
+        return $this->hasOne(Anamnese::class, 'anamnese_id');
+    }
+
+}
