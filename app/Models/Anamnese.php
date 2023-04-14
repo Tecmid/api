fd746e7a39f19807c4f80ccc9c6d6f438c11fbfd
Douglas Sales <douglas.sales@multipedidos.com.br>
2023-04-14 00:35:47 -0300

    Create anamnese model

---
+++ app/Models/Anamnese.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Models;
+
+class Anamnese
+{
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'patient_id',
+        'complaint',
+        'sickness',
+        'conduct',
+        'height',
+        'weight',
+        'blood_pressure',
+        'heart_rate',
+        'hypothesis',
+        'notes',
+    ];
+
+    /**
+     * Get appointments associated with the anamnese
+     * 
+     * @return HasMany
+     */
+    public function appointments(): HasMany
+    {
+        return $this->hasMany(Appointment::class, 'anamnese_id');
+    }
+
+}
